39afa4997af7a07acf2e31649775a14858ac0282
StackTony <33832838+StackTony@users.noreply.github.com>
2020-02-27 11:17:24 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+class Config(object):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+    SECRET_KEY = '66666'
+    # SQLALCHEMY_ECHO = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JSON_AS_ASCII = False  # 这个配置可以确保http请求返回的json数据中正常显示中文
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:lkj0519ok@localhost:3306/lkj?charset=utf8mb4"  # mysql+pymysql://用户名:密码@localhost:3306/数据库名?charset=utf8mb4
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
---
+++ manage.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#项目运行
+from app import create_app
+
+app = create_app()
+
+
+if __name__ == '__main__':
+    app.run()
